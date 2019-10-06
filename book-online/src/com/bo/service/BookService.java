5bac66794678c18c6c374f315a29615886dfcd96
mqxu <moqi1977@gmail.com>
2019-10-06 16:56:04 +0800

    大作业

--- book-online/src/com/bo/service/BookService.java
+++ book-online/src/com/bo/service/BookService.java
@@ -18,19 +18,18 @@ public class BookService {
 //    public static List<Book> init() {
 //        List<Book> bookList = new ArrayList<>(10);
 //        Book[] books = {
-//                new Book(1, "漫长的婚约", "book1.jpg", "[法] 塞巴斯蒂安"),
-//                new Book(2, "庸人自扰", "book2.jpg", "[英]戴伦•麦加维"),
-//                new Book(3, "绿山墙的安妮", "book3.jpg", "[美]理查德·耶茨"),
-//                new Book(4, "敌人与邻居", "book4.jpg", "[英]伊恩·布莱克"),
-//                new Book(5, "哀伤纪", "book5.jpg", "钟晓阳"),
-//                new Book(6, "漫长的婚约", "book1.jpg", "[法] 塞巴斯蒂安"),
-//                new Book(7, "庸人自扰", "book2.jpg", "[英]戴伦•麦加维"),
-//                new Book(8, "绿山墙的安妮", "book3.jpg", "[美]理查德·耶茨"),
-//                new Book(9, "敌人与邻居", "book4.jpg", "[英]伊恩·布莱克"),
-//                new Book(10, "哀伤纪", "book5.jpg", "钟晓阳"),
-//                new Book(11, "漫长的婚约", "book1.jpg", "[法] 塞巴斯蒂安"),
-//                new Book(12, "庸人自扰", "book2.jpg", "[英]戴伦•麦加维")
-//
+//               new Book(1,"《陈情令》", "1.jpg", "墨香铜臭"),
+//                new Book(2,"《一个人不要怕》", "2.jpg", "素黑"),
+//                new Book(3,"《摆渡人》", "3.jpg", "克莱尔麦克福尔"),
+//                new Book(4,"《战争与和平》", "4.jpg", "列夫托尔斯泰"),
+//                new Book(5,"《悲伤逆流成河》", "5.jpg", "郭敬明"),
+//                new Book(6,"《生活，是很好玩的》", "6.jpg", "汪曾祺"),
+//                new Book(7,"《活着》", "7.jpg", "余华"),
+//                new Book(8,"《夜色温柔》", "8.jpg", "费朗西斯斯科特"),
+//                new Book(9,"《有匪》", "9.jpg", "priest"),
+//                new Book(10,"《最好的我们》", "10.jpg", "八月长安"),
+//                new Book(11,"《花田半亩》", "11.jpg", "田维"),
+//                new Book(12,"《人间失格》", "12.jpg", "太宰治"),
 //        };
 //        bookList = Arrays.asList(books);
 //        return bookList;
